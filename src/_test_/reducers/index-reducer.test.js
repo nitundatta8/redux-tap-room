79579c1c5481aef5e05009e79f59bbd8decd6262
nitunwa <nitun.2@gmail.com>
2020-07-10 14:46:33 -0700

    fix the combine reducer default state  test case,problem was a typo, now rootReducer default state working successfully

--- src/_test_/reducers/index-reducer.test.js
+++ src/_test_/reducers/index-reducer.test.js
@@ -3,8 +3,10 @@ import rootReducer from '../../reducers/index';
 describe("rootReducer", () => {
 
   test('Should return default state if no action type is recognized', () => {
+    console.log(rootReducer({}, { type: null }));
+
     expect(rootReducer({}, { type: null })).toEqual({
-      masterKegList: {},
+      masterkegList: {},
       formVisibleOnPage: false
     });
   });
---
+++ src/reducers/index.js
@@ -0,0 +1,21 @@
+// import formVisibleReducer from './form-visible-reducer';
+// import kegListReducer from './keg-list-reducer';
+// import { combineReducers } from 'redux';
+
+// const rootReducer = combineReducers({
+//   formVisibleOnPage: formVisibleReducer,
+//   masterkegList: kegListReducer
+// });
+
+// export default rootReducer;
+
+import formVisibleReducer from './form-visible-reducer';
+import kegListReducer from './keg-list-reducer';
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+  formVisibleOnPage: formVisibleReducer,
+  masterkegList: kegListReducer
+});
+
+export default rootReducer;
