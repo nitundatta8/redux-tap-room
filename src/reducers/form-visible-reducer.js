468ea2e1e643b67f580a4aac7351b06d915f886a
nitunwa <nitun.2@gmail.com>
2020-07-10 12:59:57 -0700

    successfully TOGGLE_FORM action type test case pass and also add test case for rootReducer to check default state, its fails

--- src/reducers/form-visible-reducer.js
+++ src/reducers/form-visible-reducer.js
@@ -1,5 +1,10 @@
-
+import * as c from './../actions/ActionTypes';
 
 export default (state = false, action) => {
-  return state;
+  switch (action.type) {
+    case c.TOGGLE_FORM:
+      return !state;
+    default:
+      return state;
+  }
 };
