ded850a3d72f50338554e9a4411bb540f2c7a825
nitunwa <nitun.2@gmail.com>
2020-07-10 09:57:33 -0700

    add 2nd test case to check new keg is added to the masterKegList and it fails

--- src/_test_/reducers/keg-list-reducer.test.js
+++ src/_test_/reducers/keg-list-reducer.test.js
@@ -1,7 +1,44 @@
 import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('kegListReducer', () => {
+  let action;
+  const kegData = {
+    name: 'xyz',
+    brand: 'abc',
+    price: 8,
+    flavor: 'lemon',
+    caffeine: 3,
+    quantity: 6,
+    id: 1
+  }
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
+  });
+
+  test('Should successfully add new keg data to masterkegList', () => {
+    const { name, brand, price, flavor, caffeine, quantity, id } = kegData;
+    action = {
+      type: 'ADD_KEG',
+      name: name,
+      brand: brand,
+      price: price,
+      flavor: flavor,
+      caffeine: caffeine,
+      quantity: quantity,
+      id: id
+    };
+
+    expect(kegListReducer({}, action)).toEqual({
+      [id]: {
+        name: name,
+        brand: brand,
+        price: price,
+        flavor: flavor,
+        caffeine: caffeine,
+        quantity: quantity,
+        id: id
+      }
+    });
   });
 });
