446aa110a1b06e6e206130c5c394a40b849c4a65
nitunwa <nitun.2@gmail.com>
2020-07-10 15:11:47 -0700

    add test case with action type to check  initial state of formVisibleReducer and masterKegListmatches with the root reducer

--- src/_test_/reducers/index-reducer.test.js
+++ src/_test_/reducers/index-reducer.test.js
@@ -2,6 +2,7 @@ import rootReducer from '../../reducers/index';
 import { createStore } from 'redux';
 import formVisibleReducer from '../../reducers/form-visible-reducer';
 import kegListReducer from '../../reducers/keg-list-reducer';
+import * as c from '../../actions/ActionTypes';
 
 let store = createStore(rootReducer);
 
@@ -21,6 +22,30 @@ describe("rootReducer", () => {
   test('Check that initial state of formVisibleReducer matches root reducer', () => {
     expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
   });
+
+  test('Check that initial state of kegListReducer matches root reducer', () => {
+    const action = {
+      type: c.ADD_KEG,
+      name: 'xyz',
+      brand: 'abc',
+      price: 8,
+      flavor: 'lemon',
+      caffeine: 3,
+      quantity: 6,
+      id: 1
+    }
+    store.dispatch(action);
+    expect(store.getState().masterKegList).toEqual(kegListReducer(undefined, action));
+  });
+
+  test('Check that initial state of formVisibleReducer matches root reducer', () => {
+    const action = {
+      type: c.TOGGLE_FORM
+    }
+    store.dispatch(action);
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
+  });
+
 
 
 });
